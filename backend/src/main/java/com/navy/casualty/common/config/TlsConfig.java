596062b3bf35f4dcac088b2131aaba6e7e8da88a
lapiogga <65578781+lapiogga@users.noreply.github.com>
2026-04-04 17:59:25 +0900

    feat(08-02): 보안 헤더 + TLS 설정 + Actuator 제한 + 운영 프로파일
    - SecurityConfig에 HSTS, X-Content-Type-Options, X-Frame-Options 헤더 추가
    - TlsConfig: prod 프로파일 + SSL 활성화 시 HTTP->HTTPS 리다이렉트
    - application-prod.yml: HikariCP, 로그 롤링, 세션 보안 쿠키, Actuator 제한
    - application.yml: Actuator health만 외부 노출

---
+++ backend/src/main/java/com/navy/casualty/common/config/TlsConfig.java
@@ -0,0 +1,48 @@
+package com.navy.casualty.common.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * TLS 설정 - 운영 환경에서 SSL 활성화 시 HTTP->HTTPS 리다이렉트.
+ * server.ssl.enabled=true 일 때만 활성화된다.
+ */
+@Configuration
+@Profile("prod")
+@ConditionalOnProperty(name = "server.ssl.enabled", havingValue = "true")
+public class TlsConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() {
+        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
